e014dd75255c7ac3093c1f8901102c81e7ca6171
ealecho <elshore32@gmail.com>
2022-08-10 11:45:59 +0300

    unionandintersectiontypes

--- union_types.ts
+++ union_types.ts
@@ -1,14 +1,44 @@
-function filpCoin() : "heads" | "tails" {
-    if (Math.random() > 0.5) return "heads"
-    return "tails"
-}
+//UNION & INTERSECTION Types
 
-function maybeSumin() : 
+//Union Types are OR for types. They are described in Typescript using the | (pipe) operator
+//e.g if we had a type type that could be one of two strings, "success" or "error" we could define it as "success" | "error"
+//Intersections Types are AND for types. 
+
+//Dear future Edwin for give me for utilizing the conditional (tenary)? operator and arrow functions. const foo = () => it hurts readbilty
+const filpCoin = () : "heads" | "tails" => Math.random() > 0.5? "heads": "tails" 
+
+const maybeSumin = () : 
     | ["error", Error]
-    | ["success", {name: string; email: string}] {
-         return filpCoin() === "heads" ? ["success", {name: "william", email: "wil@yandex.ru"}] : ["error", new Error("The coin has landed on TAILS: (")]
+    | ["success", {name: string; email: string}]  => (filpCoin() === "heads") ? ["success", {name: "william", email: "wil@yandex.ru"}] : ["error", new Error("The coin has landed on TAILS: (")]
+
+
+const outcome = maybeSumin()
+
+
+const [first, second] = outcome;
+
+// type guards ie. using instance of to separate two potential possibilites for our value
+const data = second instanceof Error ? second : second
+
+
+//discriminated unions
+//typescript understands that the first and second positions of our tuple are linked
+const data2 = outcome[0] === "error" ? outcome : outcome
+
+
+// Intersection Types
+// these can be described with the & operator
+// eg.
+
+const ONE_WEEK = 1000 * 60 * 60 * 24 * 7
+const makeWeek = (): Date & {end: Date} => {
+    const start = new Date()
+    const end = new Date(start.valueOf() + ONE_WEEK)
+
+    return {...start, end}
 }
 
+const thisWeek = makeWeek()
+thisWeek.end.toISOString
 
-
-
+console.table([thisWeek.end.toISOString()])
